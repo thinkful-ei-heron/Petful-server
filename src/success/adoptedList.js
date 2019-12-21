d140e4229955020f86d754332c324ff80e078cd8
Glaiza Wagner <glaizawagner@gmail.com>
2019-12-21 16:31:29 -0500

    added id

--- src/success/adoptedList.js
+++ src/success/adoptedList.js
@@ -1,11 +1,13 @@
 let success = [
     {
+        id: 1,
         animalName: 'Lucky',
         imgUrl: 'https://images.dog.ceo/breeds/terrier-norwich/n02094258_1340.jpg',
         imgDesc: 'Golden norwich terrier',
         ownerName: 'Wesley',
     },
     {
+        id: 2,
         animalName: 'Lou',
         imgUrl: 'https://www.pexels.com/photo/adorable-animal-blur-cat-617278/',
         imgDesc: 'She is a sweet, green-eyed, striped tabby cat with white bib and white paws',
