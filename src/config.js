d32f22a2a9e6acc54205ad46a9beb002e716a33c
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-12-21 16:02:32 -0800

    debugging

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://enigmatic-atoll-38894.herokuapp.com',
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
 };
